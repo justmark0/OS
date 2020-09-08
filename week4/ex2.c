0ead8cdb19b7f1c81cb20d9f006ed52159e99932
Mark Nicholson <wmark.nicholsonu@gmail.com>
2020-09-08 14:46:30 +0300

    Update ex2.c

--- week4/ex2.c
+++ week4/ex2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-// start  this program with as backgroung process using &
-// and run pstree as was mwntioned in task
+// start  this program as backgroung process using &
+// and run pstree as was mentioned in the task
 int main() {
   int pid;
   for (int i = 0; i < 3; i++)
@@ -10,7 +10,7 @@ int main() {
   sleep(5);
   return 0;
 }
-// this program created 8 processes when i < 3 because on each iteration it fork (start one new process)
-// And each new process will start one more, so number of such processes is power of 2
+// This program created 8 processes when i < 3 because on each iteration it forked
+// And each new process will start one more(using fork), so number of such processes is power of 2
 
 // When i < 5 it will create 32 processes because of the same reason
