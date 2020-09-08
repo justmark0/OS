2855ef5dc203f0ca927e36be2da99481104c1ff9
Mark Nicholson <wmark.nicholsonu@gmail.com>
2020-09-08 19:38:41 +0300

    Update ex4.c

--- week4/ex4.c
+++ week4/ex4.c
@@ -3,12 +3,14 @@ int input(char *s,int length);
 
 int main()
 {
-    char buffer[32];
-    char *b = buffer;
-    size_t bufsize = 32;
-    size_t characters;
-    characters = getline(&b,&bufsize,stdin);
-    system(buffer);
+    while(1) {
+        char buffer[32];
+        char *b = buffer;
+        size_t bufsize = 32;
+        size_t characters;
+        characters = getline(&b,&bufsize,stdin);
+        system(buffer);
+    }
     return(0);
 }
 
